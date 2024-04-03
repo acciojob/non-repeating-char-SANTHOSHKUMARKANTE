fc227f3546c8d415358d2829056ec63deb07ff74
~~SANTHOSH <119999002+SANTHOSHKUMARKANTE@users.noreply.github.com>
2024-04-03 16:57:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-function firstNonRepeatedChar(str) {
- // Write your code here
+function countCharacters(str) {
+    const charCount = {};
+    for (let char of str) {
+        if (charCount[char]) {
+            charCount[char]++;
+        } else {
+            charCount[char] = 1;
+        }
+    }
+    for (let count of str){
+        if (charCount [count] == 1){
+            return count;
+        }
+    }
+	return null;
+	
+    
 }
+
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
